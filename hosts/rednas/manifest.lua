2022b8f82f4a223f77fbc412ac2da9172b4746eb
ephreal <contact@superfake.email>
2025-09-19 11:54:44 -0700

    updates hosts

--- hosts/rednas/manifest.lua
+++ hosts/rednas/manifest.lua
@@ -6,6 +6,16 @@
             filename="ensureBasalt.lua",
             savepath="/scripts/",
             serverpath="/scripts/ensureBasalt.lua"
+        },
+        {
+            filename="startup",
+            savepath="/",
+            serverpath="/hosts/rednas/startup"
+        },
+        {
+            filename="runner",
+            savepath="/",
+            serverpath="/hosts/rednas/runner"
         }
     },
     modules = {
